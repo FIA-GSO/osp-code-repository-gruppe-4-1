9ea2ceb97f95d9c8d15de15d8124f98414ca7872
Niklas Fink <niklas.f37@gso.schule.koeln>
2026-01-26 16:25:37 +0100

    feat: template inheritance added

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 """
 Marketplace GSO – Aussteller-Anmelde-Portal für die Jobmesse des Georg-Simon-Ohm-Berufskollegs
 """
-from flask import Flask
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -12,7 +11,7 @@ def landing_page():
     """
 Globaler Einstiegspunkt, insbesondere für nicht authentifizierte Nutzer
     """
-    return 'Hello World!'
+    return render_template('baseHomeContent.html')
 
 
 if __name__ == '__main__':
