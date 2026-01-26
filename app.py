5bccb0ba60bcab27db092754b18fe8e01516a581
rune.wethmar <rune.wethmar@nx-technologies.com>
2026-01-26 14:40:38 +0100

    fix: correct file structure and added missing favicon

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 Marketplace GSO – Aussteller-Anmelde-Portal für die Jobmesse des Georg-Simon-Ohm-Berufskollegs
 """
 from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
@@ -11,6 +13,8 @@ def landing_page():
     """
 Globaler Einstiegspunkt, insbesondere für nicht authentifizierte Nutzer
     """
+def hello_world():  # put application's code here
+    return render_template("baseTemplate.html")
     return 'Hello World!'
 
 
