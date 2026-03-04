240ad79682c095a1046d3a9c50c90908240314f7
Nick Seufert <nicolas.seufert@gmail.com>
2026-03-04 22:06:47 +0100

    BUG: Bei Anmeldung nicht benötigtes Datenschutzzustimmungsfeld ignorieren

--- input.py
+++ input.py
@@ -3,7 +3,7 @@ Eingabe-Validations und -Transformations-Werkzeuge hier
 """
 from typing import Any
 from werkzeug.datastructures import MultiDict
-from database.models import Booking
+from database.models import Booking, User
 
 
 class ValidationError(Exception):
@@ -19,13 +19,28 @@ def validate_booking(**kwargs) -> Booking:
     Validiert einen einzufügenden Anmeldungs-Datensatz und bereitet ihn auf.
     :param kwargs: Unverarbeitete Formulardaten (!)
     :return: fertig bestücktes Booking-Objekt zum Einfügen in DB
-    :raises: ValidationError
+    :raises: ConstraintViolation
     """
+    # ToDo: check for complete validation
     kwargs['first'] = kwargs.pop('first_day', 'no') == 'yes'
     kwargs['second'] = kwargs.pop('second_day', 'no') == 'yes'
     if not kwargs['first'] and not kwargs['second']:
         raise ConstraintViolation('Sie müssen an mindestens einem Tag teilnehmen!')
     return Booking(**kwargs)
+
+
+def preprocess_user(**fields) -> User:
+    """
+    Validiert einen einzufügenden Nutzer-Datensatz und bereitet ihn auf.
+    :param fields: Unverarbeitete Formulardaten (!)
+    :return: fertig bestücktes User-Objekt zum Einfügen in DB
+    :raises: ConstraintViolation
+    """
+    # ToDo: validate properly
+    del fields['dsgvo']
+    fields['support_association'] = fields.get('support_association') == 'on'
+
+    return User(**fields)
 
 
 def transform_filters(qparams: MultiDict[str, str]) -> dict[str, Any]:
