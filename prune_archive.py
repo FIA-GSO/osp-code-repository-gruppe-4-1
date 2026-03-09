6fb164c214ae15afb27a4d2ebe172de597a1c257
Cyenia <35280763+Cyenia@users.noreply.github.com>
2026-03-09 12:08:27 +0100

    feat: implement logging for user pruning process

--- prune_archive.py
+++ prune_archive.py
@@ -1,12 +1,28 @@
 """
 Marketplace GSO – Archiv-Bereinigungsskript für die Jobmesse des Georg-Simon-Ohm-Berufskollegs
 """
+import logging
+from logging.handlers import SysLogHandler, RotatingFileHandler
+from os import makedirs
+from sys import platform
 from datetime import datetime, timedelta
 
 from sqlalchemy import and_
 
 from database.models import Token, User, Booking
 from db import db
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+if platform.startswith('linux'):
+    syslog_handler = SysLogHandler(address='/dev/log')
+    logger.addHandler(syslog_handler)
+
+makedirs('logs', exist_ok=True)
+file_handler = RotatingFileHandler('logs/prune.log')
+logger.addHandler(file_handler)
 
 
 def prune_archive():
@@ -22,6 +38,9 @@ def prune_archive():
         active_user_count = db.query(Token).filter(same_user_filter).count()
 
         if active_user_count == 0:
+            message = f"Pruning user '{token.user.name}' (ID {token.user_id}) due to inactivity since {token.last_seen}"
+            logger.info(message)
+
             db.query(Token).filter(Token.user_id == token.user_id).delete()
             db.query(Booking).filter(Booking.user_id == token.user_id).delete()
             db.query(User).filter(User.id == token.user_id).delete()
